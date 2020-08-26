2adfc2bf6fe7bfebd4dea37bce881c8662a26faa
coursar <Sonia@MacBook-Air-Sofa.local>
2020-08-26 10:09:37 +0300

    Test for checkbox added

--- src/test/java/ru/netology/UnhappyPath.java
+++ src/test/java/ru/netology/UnhappyPath.java
@@ -1,3 +1,5 @@
+package ru.netology;
+
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
@@ -6,10 +8,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class UnhappyPath {
+
     @Test
     public void shouldNotSubmitRequestWithoutName(){
-        open("http://localhost:9999");
 
+        open("http://localhost:9999");
         SelenideElement form = $("[id=root]");
         form.$("[name=phone]").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
@@ -19,12 +22,24 @@ public class UnhappyPath {
 
     @Test
     public void shouldNotSubmitRequestWithoutPhone(){
-        open("http://localhost:9999");
 
+        open("http://localhost:9999");
         SelenideElement form = $("[id=root]");
         form.$("[name=name]").setValue("Софья");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
         form.$("[data-test-id=phone] [class = input__sub]").shouldHave(exactText("Поле обязательно для заполнения"));
     }
+
+    @Test
+    public void shouldNotSubmitRequestWithoutCheckbox(){
+
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[name=name]").setValue("Софья");
+        form.$("[name=phone]").setValue("+79270000000");
+        form.$("button").click();
+        form.$("#root > div > form > div:nth-child(3) > label").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
 }
+
